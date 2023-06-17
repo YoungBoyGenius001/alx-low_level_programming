c1402232c2e32e70a61383026f785821c9280d45
YoungBoyGenius001 <iwujulian@gmail.com>
2023-06-17 04:46:51 -0700

    mad

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,11 +6,11 @@
 * Return: Always 0 (Success)
 */
 int main(void)
-{       
+{
 	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
-	   
-	for (i = 0; i < 26; i**)
+
+	for (i = 0; i < 26; i * * )
 	{
 		putchar(alp[i]);
 	}
--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -6,10 +6,10 @@
 * Return: Always 0 (Success)
 */
 int main(void)
-{       
+{
 	int i;
-	   
-	for (i = 0; i < 10; i**)
+
+	for (i = 0; i < 10; i * *)
 	{
 		printf("%d", i);
 	}
