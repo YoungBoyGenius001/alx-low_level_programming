ac20616d7fa689f43ac981b19848f44df1059f43
YoungBoyGenius001 <iwujulian@gmail.com>
2023-06-17 03:22:05 -0700

    fin

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,10 +9,10 @@
 * Return: Always 1
 */
 int main(void)
-{       
-        char *mess = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+{
+	char *mess = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(2, mess, strlen(mess));
 
 	return (1);
-}  
+}
